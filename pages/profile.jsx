14f7a67289598359d1936944c4bc9cb1b307e83a
Alextxnk <alexsolov766@gmail.com>
2023-04-18 00:56:45 +0300

    first commit

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,44 +1,48 @@
-import { useSession, signIn, signOut } from "next-auth/react"
-import { unstable_getServerSession } from "next-auth/next"
-import { authOptions } from "./api/auth/[...nextauth]"
+import { useSession, signIn, signOut } from 'next-auth/react';
+import { unstable_getServerSession } from 'next-auth/next';
+import { authOptions } from './api/auth/[...nextauth]';
 
 export default function Component() {
-  const { data: session } = useSession()
+   const { data: session } = useSession();
 
-  if (session) {
-    return (
+   if (session) {
+      return (
+         <>
+            Signed in as {session.user.email} <br />
+            <img src={session.user.image} /> <br />
+            {session.user.name} <br />
+            <button onClick={() => signOut()}>Sign out</button>
+         </>
+      );
+   }
+   return (
       <>
-        Signed in as {session.user.email} <br />
-        <img src={session.user.image} /> <br />
-        {session.user.name} <br />
-        <button onClick={() => signOut()}>Sign out</button>
+         Not signed in <br />
+         <button onClick={() => signIn()}>Sign in</button>
       </>
-    )
-  }
-  return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
-  )
+   );
 }
 
 export async function getServerSideProps(context) {
-  const session = await unstable_getServerSession(context.req, context.res, authOptions)
+   const session = await unstable_getServerSession(
+      context.req,
+      context.res,
+      authOptions
+   );
 
-  if (!session) {
-    //redirect to login page
-    return {
-      redirect: {
-        destination: "/api/auth/signin",
-        permanent: false,
-      },
-    }
-  }
+   if (!session) {
+      //redirect to login page
+      return {
+         redirect: {
+            destination: '/api/auth/signin',
+            permanent: false
+         }
+      };
+   }
 
-  return {
-    props: {
-      session,
-    },
-  }
+   return {
+      props: {
+         session
+      }
+   };
 }
